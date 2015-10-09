6617edc0fd6f66a5ca7496262cfc2bc284617453
wrensf <rena@projectbly.com>
2015-10-09 04:13:16 +0000

    Added homework calculator

---
+++ RenaThiagarajan_wrensf/calculator.rb
@@ -0,0 +1,50 @@
+
+class Calculator
+    def initilize
+    puts "enter your first number."
+    @num1 = gets.chomp.to_i
+    puts "enter your sencond number."
+    @num2 = gets.chomp.to_i 
+    end
+    
+    def add_two 
+    puts @num1 + @num2
+    end
+    
+    def divide
+    puts @num1 / @num2
+    end
+    
+    def subtract 
+    puts @num1 - @num2
+    end
+    
+    def multiply 
+    puts @num1 * @num2
+    end
+    
+    puts "enter your operation: multiply, divide, add, subtract"
+    @operation = gets.chomp
+    end
+    
+    def run_calculator
+        if @operation == "multiply"
+        puts @num1 * @num2
+        
+        elsif @operation == "add"
+        puts @num1 + num2
+        
+        elsif @operation == "subtract"
+        puts @num1 - @num2
+        
+        else @operation == "divide"
+        puts @num1 / num2
+        
+        end
+    
+    
+end
+
+myCalc = Calculator.new
+mycalc.multiply
+mycalc.divide
