707a15c91f5166f0a41cac8ff3b56ae0925da076
Inga Chen <inga.h.chen@gmail.com>
2015-10-09 04:10:03 +0000

    Made an edit to the homework

--- IngaChen_ingachen/hw_1/calculator.rb
+++ IngaChen_ingachen/hw_1/calculator.rb
@@ -4,9 +4,9 @@ class Calculator
         @num1 = gets.chomp.to_i
         puts "Give me a second random number"
         @num2 = gets.chomp.to_i
-        
         puts "Enter your operation: log, sine, square root, tangent"
         @operation = gets.chomp 
+    end
         
     def run_calculator
         if @operation == "log"
@@ -17,29 +17,31 @@ class Calculator
             puts Math.sqrt(@num1)
         else
             puts Math.tan(@num1)
-    end
-        
-    end
-    
-    def logarithm
-        puts Math.log(@num1,@num2)
-    end
-    
-    def sine
-        puts Math.sin(@num1)
-    end
-    
-    def square_root
-        puts Math.sqrt(@num2)
-    end
-    
-    def tangent
-        puts Math.tan(@num1)
+        end
     end
 end
+    
+#     def logarithm
+#         puts Math.log(@num1,@num2)
+#     end
+    
+#     def sine
+#         puts Math.sin(@num1)
+#     end
+    
+#     def square_root
+#         puts Math.sqrt(@num2)
+#     end
+    
+#     def tangent
+#         puts Math.tan(@num1)
+#     end
+
+# myCalc = Calculator.new
+# myCalc.logarithm
+# myCalc.sine
+# myCalc.square_root
+# myCalc.tangent
 
 myCalc = Calculator.new
-myCalc.logarithm
-myCalc.sine
-myCalc.square_root
-myCalc.tangent
+myCalc.run_calculator
