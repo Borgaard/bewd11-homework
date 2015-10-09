3d1ceb567163c42d2fa41653f6369941961ffd09
Lemures <msmith3dart@icloud.com>
2015-10-09 03:59:10 +0000

    uploading my directory

---
+++ MartinSmith-Lemures/hw1/Calulator.rb
@@ -0,0 +1,29 @@
+class Calculator
+   def initialize
+        puts "add a value for x"
+        @valueX = gets.chomp.to_i
+        puts "add a value for y"
+        @valueY = gets.chomp.to_i
+        
+        puts "Ender your operatoin: multiply, Divide, add, subtract"
+        
+        @operation = gets.chomp
+   end
+ 
+    def run_calculator
+        if @operation == "multiply"
+            puts @valueX * @valueY
+        elsif @operation == "divide"
+            puts @valueX / @valueY
+        elsif operation == "add"
+            puts @valueX + @valueY
+        elsif operation == "subtract"
+            puts @valueX - @valueY
+        end     
+    end
+
+end
+
+myCalc = Calculator.new
+
+myCalc.run_calculator
