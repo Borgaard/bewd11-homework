6315fa28935c69a9a038e11b227d6d8e63e4482d
Haley McLaughlin <mcgloflen@gmail.com>
2015-10-21 01:24:27 +0000

    homeowork 2

---
+++ HaleyMcLaughlin-mcgloflen/hw_3/homework.rb
@@ -0,0 +1,5 @@
+user_names = ["Bob Jones", "Cindy Smith", "Kelsey Scott"]
+
+user_emails = ["bob@gmail.com", "csmith@yahoo.com", "kscott@hotmail.com"]
+
+#missed class on thurs, so will complete this hw this week!
