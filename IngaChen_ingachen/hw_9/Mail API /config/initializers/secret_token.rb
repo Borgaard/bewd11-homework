4b8b6670b22fe61f70f85557cab71ecbb8a992ac
ingachen <inga.h.chen@gmail.com>
2015-12-08 00:44:40 -0800

    Week 9 hw edits

---
+++ IngaChen_ingachen/hw_9/Mail API /config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Api::Application.config.secret_token = '4280b0bff0c992ae18db05fb83808a3c84db1648c88445b95d6c1ef89f808b4d274f87fa0ce4dec854d80541a8caed34e00225cad011d474135fdef7fcbf76c2'
